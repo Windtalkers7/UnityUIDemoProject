cc9d021e0584d4e3034616abcf1da6ba362ea91c
Windtalkers <18819270171@163.com>
2017-10-31 17:14:32 +0800

    add TestAddScript

---
+++ Assets/dingxiaowei-scrollrect-master/scrollrect/Assets/Scripts/TestAddScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAddScript : MonoBehaviour
+{
+    void TestCommitCode()
+    {
+        //UIDemo工程下提交的代码
+        Debug.Log("提交者UIDemo");
+    }
+}
